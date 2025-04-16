be32785655ff1300221b1c02fc61f8b268f67e3a
BogdanR6 <bogdanrares732@gmail.com>
2025-04-16 22:05:35 +0300

    Add shell commands exit and cd and error handling

--- main.rs
+++ main.rs
@@ -1,19 +1,39 @@
+use std::env;
 use std::io::{self, Write};
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
     loop {
         print!("> ");
-        io::stdout().flush();
+        let _ = io::stdout().flush();
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
         let mut args = input.trim().split_whitespace();
         let command = args.next().unwrap();
+        match command {
+            "cd" => {
+                // default to '/' as new directory if one was not provided
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            }
+            "exit" => return,
+            command => {
+                let child = Command::new(command).args(args).spawn();
 
-        let mut child = Command::new(command).args(args).spawn().unwrap();
-
-        child.wait();
+                // gracefully handle malformed user input
+                match child {
+                    Ok(mut child) => {
+                        let _ = child.wait();
+                    }
+                    Err(e) => eprintln!("{}", e),
+                };
+            }
+        }
     }
 }
