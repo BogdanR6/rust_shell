bc313e663d85fc4a0d2c76a9e1048a395a5cb109
BogdanR6 <bogdanrares732@gmail.com>
2025-04-16 20:54:40 +0300

    Initial commit

---
+++ main.rs
@@ -0,0 +1,19 @@
+use std::io::{self, Write};
+use std::process::Command;
+
+fn main() {
+    loop {
+        print!("> ");
+        io::stdout().flush();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let mut args = input.trim().split_whitespace();
+        let command = args.next().unwrap();
+
+        let mut child = Command::new(command).args(args).spawn().unwrap();
+
+        child.wait();
+    }
+}
